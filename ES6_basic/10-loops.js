d62fd44480ce020465f164b3c06f57da64cbd0f9
Arif <116842869+ArifAlakbarovv@users.noreply.github.com>
2024-08-04 11:10:49 +0400

    Create 10-loops.js

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    for (const value of array) {
+        const idx = array.indexOf(value);
+        array[idx] = appendString + value;
+    }
+    
+    return array;
+}
